4462fc24dda82e8ab81a623bff6285abb2911065
Emilio Pomares <remilia@Emilios-iMac.local>
2018-11-28 13:05:06 +0100

    sdlfjk

--- src/server/main.go
+++ src/server/main.go
@@ -9,12 +9,29 @@ G+: https://plus.google.com/u/0/105524772414753584405/
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
+	"math"
 	"net"
+//	"github.com/gordonklaus/portaudio"
 )
 
+var readBank = 0
+var writeBank = 0
+
+const nBanks = 4
+
+const bufferSize = 96
+
+const sampleRate = 8000
+const bytesPerSample = 2
+
+var availableFrames = 0
+
+var packetsReceived = 0
+
 func main() {
+
+	// networking
 
 	//Basic variables
 	port := ":8080"
@@ -36,23 +53,62 @@ func main() {
 		fmt.Println(err)
 	}
 
+	buffer := make([]int16, bufferSize * nBanks)
+        for i:= range buffer {
+                buffer[i] = int16(2000.0*math.Sin(float64(i)/3.0))
+         }
+/*
+	// audio
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+
+
+	stream, err := portaudio.OpenDefaultStream(0, 1, sampleRate, bufferSize,
+     		func(out []int16) {
+			if availableFrames > 0 {
+				for i:=range out {
+					out[i] = buffer[i+bufferSize*(availableFrames-1)]
+				}
+				availableFrames--
+				return
+			}
+			for i:=range out {
+				out[i] = 0
+			}
+		})
+	stream.Start()
+	defer stream.Close()
+
+	fmt.Printf("frames: %d\n", availableFrames)
+*/
 	//Keep calling this function
 	for {
-		display(udpConn)
+		fmt.Println("listening for frame...")
+		addData(udpConn, buffer)
 	}
 
 }
 
-func display(conn *net.UDPConn) {
+func addData(conn *net.UDPConn, sharedBuffer []int16) {
 
 	var buf [2048]byte
 	n, err := conn.Read(buf[0:])
+	if n != bufferSize * bytesPerSample {
+		fmt.Println("Packet dropped")
+		return
+	}
 	if err != nil {
 		fmt.Println("Error Reading")
 		return
 	} else {
-		fmt.Println(hex.EncodeToString(buf[0:n]))
-		fmt.Printf("Package Done, size: %d  \n", n)
+		//fmt.Println(hex.EncodeToString(buf[0:n]))
+		//fmt.Printf("Package Done, size: %d  \n", n)
+		for i := 0 ; i < bufferSize; i++ {
+			sharedBuffer[i+bufferSize*(availableFrames-1)] = int16(buf[i*2]) + int16(buf[i*2+1]) << 8
+		}
+		availableFrames++
 	}
+	packetsReceived++
+	fmt.Printf("frames: %d, packets received: %d\n", availableFrames, packetsReceived)
 
 }

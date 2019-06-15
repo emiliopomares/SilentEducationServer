2401dc7bfde8ac1843621859e8ed6fef5861f33f
Emilio Pomares <remilia@Emilios-iMac.home>
2019-06-15 13:40:49 +0200

    latest

--- src/server/client.go
+++ src/server/client.go
@@ -17,7 +17,7 @@ func main() {
     ServerAddr,err := net.ResolveUDPAddr("udp","239.255.255.250:10001")
     CheckError(err)
  
-    LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+    LocalAddr, err := net.ResolveUDPAddr("udp", "239.255.255.250:0")
     CheckError(err)
  
     Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
